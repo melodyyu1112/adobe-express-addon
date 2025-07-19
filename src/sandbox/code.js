88d18fb69d1db85dd61b33edc73971ddc5f2be7e
Melody Yu <yu.musicssis@gmail.com>
2025-07-19 14:49:45 -0400

    add splash api

--- src/sandbox/code.js
+++ src/sandbox/code.js
@@ -1,38 +1,45 @@
 import addOnSandboxSdk from "add-on-sdk-document-sandbox";
-import { editor } from "express-document-sdk";
+import {editor} from "express-document-sdk";
 
 // Get the document sandbox runtime.
-const { runtime } = addOnSandboxSdk.instance;
+const {runtime} = addOnSandboxSdk.instance;
 
 function start() {
-    // APIs to be exposed to the UI runtime
-    // i.e., to the `index.html` file of this add-on.
-    const sandboxApi = {
-        createRectangle: () => {
-            const rectangle = editor.createRectangle();
+  // APIs to be exposed to the UI runtime
+  // i.e., to the `index.html` file of this add-on.
+  const sandboxApi = {
+    createRectangle: () => {
+      const rectangle = editor.createRectangle();
 
-            // Define rectangle dimensions.
-            rectangle.width = 240;
-            rectangle.height = 180;
+      // Define rectangle dimensions.
+      rectangle.width = 240;
+      rectangle.height = 180;
 
-            // Define rectangle position.
-            rectangle.translation = { x: 10, y: 10 };
+      // Define rectangle position.
+      rectangle.translation = {x: 10, y: 10};
 
-            // Define rectangle color.
-            const color = { red: 0.32, green: 0.34, blue: 0.89, alpha: 1 };
+      // Define rectangle color.
+      const color = {red: 0.32, green: 0.34, blue: 0.89, alpha: 1};
 
-            // Fill the rectangle with the color.
-            const rectangleFill = editor.makeColorFill(color);
-            rectangle.fill = rectangleFill;
+      // Fill the rectangle with the color.
+      const rectangleFill = editor.makeColorFill(color);
+      rectangle.fill = rectangleFill;
 
-            // Add the rectangle to the document.
-            const insertionParent = editor.context.insertionParent;
-            insertionParent.children.append(rectangle);
-        }
-    };
+      // Add the rectangle to the document.
+      const insertionParent = editor.context.insertionParent;
+      insertionParent.children.append(rectangle);
+    },
+    insertImageAt: async ({url, x, y}) => {
+      const image = await editor.createImage(url);
+      image.translation = {x, y};
+      image.width = 300;
+      image.height = 300;
+      editor.context.insertionParent.children.append(image);
+    },
+  };
 
-    // Expose `sandboxApi` to the UI runtime.
-    runtime.exposeApi(sandboxApi);
+  // Expose `sandboxApi` to the UI runtime.
+  runtime.exposeApi(sandboxApi);
 }
 
 start();
